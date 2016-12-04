6a8f5f04635e4b5276acd0c2af0cebca41363201
neitsa <neitsa@gmail.com>
2016-12-04 01:53:27 +0100

    Add start file for RT-Common and save project file.

---
+++ src/RemoteTech-Common/CommonCore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RemoteTech.Common
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class CommonCoreMainMenu : CommonCore
+    {
+        public new void Start()
+        {
+            base.Start();
+        }
+    }
+
+    public abstract class CommonCore: MonoBehaviour
+    {
+        public void Start()
+        {
+            Logging.Debug("RemoteTech-Common Starting");
+        }
+    }
+}
