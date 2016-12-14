da29cc78c60862e9d8562665535bd29ddfe41398
neitsa <neitsa@gmail.com>
2016-12-15 00:11:34 +0100

    Add new class to check for other RT modules.

---
+++ src/RemoteTech-Common/Api/RemoteTechModules.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace RemoteTech.Common.Api
+{
+    public static class RemoteTechModules
+    {
+        public const string RemoteTechDelayAssemblyName = "RemoteTech-Delay.dll";
+
+        public static bool IsRemoteTechDelayLoaded()
+        {
+            return AssemblyByName(RemoteTechDelayAssemblyName) != null;
+        }
+
+        internal static AssemblyLoader.LoadedAssembly AssemblyByName(string assemblyName)
+        {
+            var assemblyCount = AssemblyLoader.loadedAssemblies.Count;
+            for (var i = 0; i < assemblyCount; i++)
+            {
+                var assembly = AssemblyLoader.loadedAssemblies[i];
+                if (assembly.name == assemblyName)
+                    return assembly;
+            }
+
+            return null;
+        }
+    }
+}
