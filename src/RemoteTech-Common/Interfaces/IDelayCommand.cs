921aeb8ee1880f10f54c0e004821607067ff74c3
neitsa <neitsa@gmail.com>
2016-12-29 11:15:06 +0100

    Add interface for delayed commands.

---
+++ src/RemoteTech-Common/Interfaces/IDelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RemoteTech.Common.Interfaces
+{
+    /// <summary>
+    ///     A command whose execution can be delayed in time.
+    /// </summary>
+    public interface IDelayCommand : ICommand, IComparable<IDelayCommand>
+    {
+        /// <summary>
+        ///     Base delay for the command.
+        /// </summary>
+        double Delay { get; }
+
+        /// <summary>
+        ///     Extra delay added to the current command <see cref="Delay" />.
+        /// </summary>
+        double ExtraDelay { get; set; }
+
+        /// <summary>
+        ///     Total delay of the command (<see cref="Delay" /> + <see cref="ExtraDelay" />).
+        /// </summary>
+        double TotalDelay { get; }
+
+        /// <summary>
+        ///     Time at which the command is planned to be executed.
+        /// </summary>
+        /// <remarks><see cref="PlannedExecutionTime" /> = <see cref="ICommand.TimeStamp" /> + <see cref="TotalDelay" /></remarks>
+        double PlannedExecutionTime { get; }
+    }
+}
