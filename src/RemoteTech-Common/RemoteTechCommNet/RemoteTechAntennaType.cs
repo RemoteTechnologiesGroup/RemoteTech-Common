efab543858b2462154c3658578c93537a0fe2f8e
KSP-TaxiService <chinglihlim@gmail.com>
2019-08-08 23:33:16 +0800

    Add new enum for all antenna types

---
+++ src/RemoteTech-Common/RemoteTechCommNet/RemoteTechAntennaType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace RemoteTech.Common.RemoteTechCommNet
+{
+    public enum RemoteTechAntennaType
+    {
+        [Description("Internal")]
+        INTERNAL = 0,
+        [Description("Dish")]
+        DISH = 1,
+        [Description("Omni")]
+        OMNI = 2,
+        [Description("Phased Array")]
+        PHASEDARRAY = 3,
+        [Description("Optical")]
+        OPTICAL = 4
+    }
+}
